8c41c17e76e67fb7b90e1b2d77c71aad28d4cadf
Dima Ovharenko <dima-o@mail.ru>
2023-01-31 12:46:22 +0300

    add all var

---
+++ classes/File.php
@@ -0,0 +1,20 @@
+<?php
+class File {
+    public $file;
+
+    public function __construct($file) {
+        $this->file = $file;
+    }
+
+    public function checkUpdate() {
+
+    }
+
+    public function clearCache() {
+        
+    }
+
+    public function readFile() {
+        
+    }
+}
---
+++ index.php
@@ -0,0 +1,5 @@
+<?
+
+require_once('classes/File.php');
+
+$file = new File(__DIR__ . '/files/check.txt');
