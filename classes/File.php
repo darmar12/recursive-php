6f1fd5a4d4bd09907be4df083012925c8a0cce3c
Dima Ovharenko <dima-o@mail.ru>
2023-01-31 16:59:24 +0300

    add fucntion readPath

--- classes/File.php
+++ classes/File.php
@@ -1,19 +1,35 @@
 <?php
 class File {
     public $file;
-    public $currentTime;
+    public $arrCurrentTime;
 
-    public function __construct($file, $currentTime) {
+    public function __construct($file, $arrCurrentTime = []) {
         $this->file = $file;
-        $this->currentTime = $currentTime;
+        $this->arrCurrentTime = $arrCurrentTime;
     }
 
     public function checkUpdate() {
         clearstatcache(false, $this->file);
         $nowTime = filemtime($this->file);
-        if ($nowTime != $this->currentTime) {
-            $this->currentTime = $nowTime;
+        if ($nowTime != $this->arrCurrentTime) {
+            $this->arrCurrentTime = $nowTime;
             echo "File was modified \n";
+        }
+    }
+
+    public function readPath($path = $this->file, &$arrFiles) {
+        $arrFiles[$path] = filemtime($path);
+        if(is_dir($path)) {
+            $files = scandir($path);
+            foreach($files as $file) {
+                if ($file === '.' || $file === '..')
+                    continue;
+                $newPath = $path . '/' . $file;
+                $arrFiles[$newPath] = filemtime($newPath);
+                if (is_dir($newPath))
+                    $this->readPath($newPath, $arrFiles);
+                
+            }
         }
     }
 }
